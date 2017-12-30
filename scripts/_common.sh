78bdb8d9ed464c6aef0ec69caf28ba171a0f27c5
artus <artus@landoftheunicorn.hd.free.fr>
2017-12-30 21:05:07 +0100

    fixes venv_* functions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,15 +2,25 @@
 
 # Setup a virtual environnement
 venv_setup() {
-	path="${final_path}/env"
+	venv_path="${final_path}/env"
 	sudo virtualenv -p python3 $path
 }
 
-venv_activate() {
-	local path="${1}/env"
-	source "${path}/bin/activate"
+venv_pip() {
+	venv_activate 
+	pip "$@"
+	venv_deactivate
 }
 
+# $venv_path must be set ! 
+# It usually is by running venv_setup
+venv_activate() {
+	source "${venv_path}/bin/activate"
+}
+
+venv_deactivate() {
+	deactivate
+}
 
 # Setup a django project
 # Arguments are:
@@ -21,7 +31,7 @@ django_setup_project() {
 	base_path=$1
 
 	# Activate virtual env
-	venv_activate $base_path
+	venv_activate
 
 	# Create django project
 	project_name="app"
@@ -35,19 +45,19 @@ django_setup_project() {
         # TODO: set up a useful admin user (like an existing one in Yunohost)	
 	# WARNING: this uses user, domain and password global variables !
 	python "${django_path}/manage.py" createsuperuser --no-input --username=$user --email="${user}@${domain}"
-	python "${django_path}/manage.py" shell -c "
-from django.contrib.auth.models import User; 
-admin = User.objects.get(username='${user}'); 
-admin.set_password('${password}'); 
+	python "${django_path}/manage.py" shell -c "\
+from django.contrib.auth.models import User; \
+admin = User.objects.get(username='${user}'); \
+admin.set_password('${password}'); \
 admin.save()"
 }
 
 # Set up custom settings
-django_setup_settings() {
+#django_setup_settings() {
 
 	#TODO: update settings in auto-generated app/settings.py file
 	# Setup allowed_hosts, secret_key and script_name
 
 	#TODO: check if a custom 'settings.py' file is in conf, if so, copy it to app/app_settings.py
 	# and use it in gunicorn service
-}
+#}
